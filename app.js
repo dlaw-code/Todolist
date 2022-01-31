642184b428c5901d181e586c36d2a6118edbf0d7
Ahmed Ajala <ajalaahmed8@gmail.com>
2022-01-31 08:30:46 +0800

    Another commit

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.post("/", function(req,res) {
 
 
 
-app.listen( process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Listening on port 3000");
 })
